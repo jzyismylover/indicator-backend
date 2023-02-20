c85b14d100ced6907313e00d92e4364f46e709e8
jzy <3011543110@qq.com>
2023-02-20 17:53:51 +0800

    feat(220): 印尼语实例

---
+++ utils/useForIndonesiaFactory.py
@@ -0,0 +1,31 @@
+import re
+import hanlp
+from utils.useForFactory import Base_Utils
+
+
+Hanlp = hanlp.load(
+    hanlp.pretrained.mtl.UD_ONTONOTES_TOK_POS_LEM_FEA_NER_SRL_DEP_SDP_CON_MMINILMV2L6
+)
+
+
+class ID_Utils(Base_Utils):
+    def get_sentences(self, text):
+        sentences = re.split(r'[?!.,]', text)
+        return sentences
+
+    def get_words(self, sentences):
+        words = []
+        tags = []
+
+        for sentence in sentences:
+            if len(sentences) == 0:
+                continue
+            ans = Hanlp(sentence, tasks='ud')
+            words.extend([x for x in ans['tok']])
+            tags.extend([x for x in ans['pos']])
+
+        self.tags = tags
+        return words
+
+    def get_word_character(self, words):
+        return self.tags
