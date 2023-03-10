d8d4803f291de63316e7d4a9d132c12c652cfa0a
jzy <3011543110@qq.com>
2023-03-10 14:29:23 +0800

    feat(308): sentry 错误监控

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from flask_cors import CORS
 from config import init_db, init_redis
 from resources import init_views
 
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+sentry_sdk.init(
+    dsn="https://828c3ed2282a49da8e8c58ca165e4d4e@o4504333117554688.ingest.sentry.io/4504795093008384",
+    integrations=[
+        FlaskIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
 app = Flask(__name__)
 
 # init global params
@@ -10,7 +24,7 @@ app.config['BUNDLE_ERRORS'] = 1
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # init db
-db = init_db(app=app)
+# init_db()
 
 # init redis
 redis_client = init_redis(app=app)
