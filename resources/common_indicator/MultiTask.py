bc615baaf416ff277da4c94e347d6f3b5e33c112
jzy <3011543110@qq.com>
2023-04-17 01:48:23 +0800

    feat: 邮件发送附件

---
+++ resources/common_indicator/MultiTask.py
@@ -0,0 +1,170 @@
+import json
+import uuid
+from io import BytesIO
+from openpyxl import Workbook
+from sqlalchemy import select
+from flask_restful import Resource, request
+from resources.Language import LanguageRec, LAN_MAPPER
+from werkzeug.datastructures import FileStorage
+from utils.jwt import check_premission
+from utils.json_response import make_success_response
+from resources.common_indicator.util import CommonIndicatorHandler
+from config._celery import celery
+from flask_mail import Message
+from config.mail import mail
+from resources.constant import (
+    COMMON_INDICATOR_HANDLER_MAPPING,
+    READABILITY_INDICATOR_HANDLER_MAPPING,
+    CUMYLATIVE_INDICATORS,
+)
+from config.db import engine
+from config.db.user import User
+
+
+language_ins = LanguageRec()
+
+
+@celery.task(name='resources.common_indicator.MultiTask.parse_files')
+def parse_files(files, email):
+    files = json.loads(files)
+    lis = []
+    for file in files:
+        file = json.loads(file)
+        filename = file['filename']
+        contentType = file['content_type']
+        # data = file['data'].encode('uft-8')
+        data = bytes.fromhex(file['data'])
+        file_storage = FileStorage(BytesIO(data), filename, content_type=contentType)
+        lg_type, lg_content = language_ins.parse_file(file_storage)
+
+        model = CommonIndicatorHandler(lg_content, lg_type)
+        ans = dict()
+        ans['filename'] = filename
+
+        indicators = COMMON_INDICATOR_HANDLER_MAPPING.keys()
+        for _ in indicators:
+            if _ not in COMMON_INDICATOR_HANDLER_MAPPING:
+                continue
+            func = getattr(model, COMMON_INDICATOR_HANDLER_MAPPING[_], None)
+            # 非增量指标累计
+            score = func()
+            ans[_] = score
+
+            lis.append(ans)
+
+    try:
+        wb = Workbook()
+        for _ in lis:
+            ws = wb.create_sheet(_['filename'])
+            ws['A1'] = '指标名'
+            ws['B1'] = '指标值'
+            i = 2
+            for row in indicators.keys():
+                if row == 'hash_value':
+                    continue
+                ws.cell(row=i, column=1).value = row
+                ws.cell(row=i, column=2).value = data[row]
+                i += 1
+
+        output = BytesIO()
+        wb.save(output)
+        output.seek(0)
+
+        message = Message(
+            subject='指标提取结果', body=f'请在附件查收指标提取结果', recipients=[email]
+        )
+        filename = f'{uuid.uuid4().__repr__()[6:-3]}.xlsx'
+        message.attach(filename, 'application/octet-stream', output.read())
+        mail.send(message)
+        return {'message': '验证码发送成功'}
+    except:
+        return {'message': '验证码发送失败'}
+
+
+class MultiProcessCommonIndicator(Resource):
+    def __init__(self) -> None:
+        self.files = request.files.getlist('files')
+
+    @check_premission
+    def post(self, info):
+        # search for user email
+        with engine.connect() as conn:
+            stmt = (
+                select(User).where(User.id == info['user_id'])
+            )
+            rows = conn.execute(stmt)
+            row = list(rows)[0]
+            email = row.email
+
+        files = []
+        for file in self.files:
+            data = {
+                'filename': file.filename,
+                'content_type': file.content_type,
+                'content_length': file.content_length,
+                # 'data': file.read().hex()
+                'data': file.read()
+            }
+            files.append(data)
+            # files.append(json.dumps(data))
+        # files = json.loads(files)
+        lis = []
+        for file in files:
+            # file = json.loads(file)
+            filename = file['filename']
+            contentType = file['content_type']
+            data = file['data']
+            file_storage = FileStorage(BytesIO(data), filename, content_type=contentType)
+            lg_type, lg_content = language_ins.parse_file(file_storage)
+
+            model = CommonIndicatorHandler(lg_content, lg_type)
+            ans = dict()
+            ans['filename'] = filename
+
+            indicators = COMMON_INDICATOR_HANDLER_MAPPING.keys()
+            for _ in indicators:
+                if _ not in COMMON_INDICATOR_HANDLER_MAPPING:
+                    continue
+                func = getattr(model, COMMON_INDICATOR_HANDLER_MAPPING[_], None)
+                # 非增量指标累计
+                score = func()
+                ans[_] = score
+
+            lis.append(ans)
+
+        try:
+            wb = Workbook()
+            for _ in lis:
+                ws = wb.create_sheet(_['filename'])
+                ws['A1'] = '指标名'
+                ws['B1'] = '指标值'
+                i = 2
+                for row in indicators:
+                    if row == 'hash_value':
+                        continue
+                    ws.cell(row=i, column=1).value = row
+                    ws.cell(row=i, column=2).value = _[row]
+                    i += 1
+
+            output = BytesIO()
+            wb.save(output)
+            output.seek(0)
+
+            message = Message(
+                subject='指标提取结果', body=f'请在附件查收指标提取结果', recipients=[email]
+            )
+            filename = f'{uuid.uuid4().__repr__()[6:-3]}.xlsx'
+            message.attach(filename, 'application/octet-stream', output.read())
+            mail.send(message)
+        except Exception as e:
+            print(e)
+        # task = parse_files.delay(json.dumps(files), email)
+        # return make_success_response(msg='success', data={'id': task.id})
+
+
+class GetStatue(Resource):
+    def get(self):
+        id = request.form['id']
+        result = parse_files.AsyncResult(id)
+        print(dir(result))
+        return {'result': result.info}
